0c0fd0d3d4a5250938cbc5b01d73b61ad9bb06d3
lucadruda <lucadruda@gmail.com>
2022-10-26 16:35:24 +0200

    update deployment script

--- deployments/scripts/config.sh
+++ deployments/scripts/config.sh
@@ -22,9 +22,9 @@ pip install --upgrade pip
 pip install -q mssql-cli
 
 # Setup DPS
-# az config set extension.use_dynamic_install=yes_without_prompt
-# az login --identity
-# az account list
+az config set extension.use_dynamic_install=yes_without_prompt
+az login --identity
+az account list
 # az iot dps enrollment-group create -g "$RESOURCE_GROUP" --dps-name "$DPSName" --enrollment-id "$ENROLLMENT_GROUP_ID" --primary-key "$ENROLLMENT_PRIMARY_KEY" --secondary-key "$ENROLLMENT_SECONDARY_KEY" --subscription "$SUBSCRIPTION_ID"
 
 # Add Azure function code
