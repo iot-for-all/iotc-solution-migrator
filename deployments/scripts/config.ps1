a0654ca39268d4ad2318380ab08d005d6d296b8d
lucadruda <lucadruda@gmail.com>
2022-10-26 15:49:19 +0200

    update script

--- deployments/scripts/config.ps1
+++ deployments/scripts/config.ps1
@@ -1,20 +1,3 @@
-param(
-    [string] $SubscriptionId
-    [string] $ResourceGroup
-    [string] $EnrollmentGroupName
-    [string] $PrimaryKey
-    [string] $SecondaryKey
-    [string] $DpsName
-    [string] $FunctionPrincipal
-)
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/lucadruda/iotc-solution-migrator/main/deployments/scripts/config.sh" -OutFile /tmp/config.sh
 
-Connect-AzAccount -Identity
-Install-Module Az.DeviceProvisioningServices -Force
-Install-Module SQLServer -Force
-Select-AzSubscription -Subscription $subscriptionId
-
-
-Add-AzIoTDeviceProvisioningServiceEnrollmentGroup -ResourceGroupName $ResourceGroup -Name $EnrollmentGroupName -AttestationType SymmetricKey -PrimaryKey "$PrimaryKey" -SecondaryKey "$SecondaryKey" -DpsName "$DpsName"
-
-# SQL setup
-Import-Module SQLServer
+bash /tmp/config.sh
